8bebc2ad9da75c6b4b1d09d0334f694fa1b50d1d
Francesco Corti <fcorti@gmail.com>
2017-04-30 22:40:08 +0200

    Chapter 3.

---
+++ Chapter 3 - Getting started with Reporting SDK/pentaho-reporting-web-app/src/main/java/com/example/PentahoServlet.java
@@ -0,0 +1,63 @@
+package com.example;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
+import org.pentaho.reporting.engine.classic.core.MasterReport;
+import org.pentaho.reporting.engine.classic.core.modules.output.pageable.pdf.PdfReportUtil;
+import org.pentaho.reporting.libraries.resourceloader.Resource;
+import org.pentaho.reporting.libraries.resourceloader.ResourceManager;
+ 
+public class PentahoServlet extends HttpServlet {
+
+  @Override
+  public void init(
+    ServletConfig config) 
+    throws ServletException {
+    super.init(config);
+    ClassicEngineBoot.getInstance().start();
+  }
+
+  @Override
+  public void doGet(
+    HttpServletRequest request, 
+    HttpServletResponse response)
+    throws ServletException, IOException {
+    doPost(request, response);
+  }
+ 
+  @Override
+  public void doPost(
+    HttpServletRequest request, 
+    HttpServletResponse response)
+    throws ServletException, IOException {
+
+    // Prpt file.
+    String reportPath = "file:" + this.getServletContext().getRealPath("WEB-INF/classes/reports/my_first_report.prpt");
+
+    try {
+
+      // Resource manager.
+      ResourceManager manager = new ResourceManager();
+      manager.registerDefaults();
+
+      // Get report.
+      Resource res = manager.createDirectly(new URL(reportPath), MasterReport.class);
+      MasterReport report = (MasterReport) res.getResource();
+
+      // Generate report in PDF into the response.
+      response.setContentType("application/pdf");
+      PdfReportUtil.createPDF(report, response.getOutputStream());
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+  }
+}
