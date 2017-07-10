9f473e18d2155e32cf12933f80d5193b77b6b396
Francesco Corti <fcorti@gmail.com>
2017-07-10 21:24:52 +0200

    Added the stati-jdbc example on chapter 12.

---
+++ Chapter 12 - Developing using data sources/pentaho-reporting-data-source-static-jdbc/src/main/java/com/example/PentahoServlet.java
@@ -0,0 +1,104 @@
+package com.example;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
+import org.pentaho.reporting.engine.classic.core.Element;
+import org.pentaho.reporting.engine.classic.core.ItemBand;
+import org.pentaho.reporting.engine.classic.core.MasterReport;
+import org.pentaho.reporting.engine.classic.core.elementfactory.NumberFieldElementFactory;
+import org.pentaho.reporting.engine.classic.core.elementfactory.TextFieldElementFactory;
+import org.pentaho.reporting.engine.classic.core.modules.misc.datafactory.sql.SQLReportDataFactory;
+import org.pentaho.reporting.engine.classic.core.modules.misc.datafactory.sql.StaticConnectionProvider;
+import org.pentaho.reporting.engine.classic.core.modules.output.pageable.pdf.PdfReportUtil;
+
+public class PentahoServlet extends HttpServlet 
+{
+
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public void init(
+    ServletConfig config) 
+    throws ServletException 
+  {
+
+    super.init(config);
+    ClassicEngineBoot.getInstance().start();
+
+  }
+
+  @Override
+  public void doGet(
+    HttpServletRequest request, 
+    HttpServletResponse response)
+    throws ServletException, IOException 
+  {
+    doPost(request, response);
+  }
+ 
+  @Override
+  public void doPost(
+    HttpServletRequest request, 
+    HttpServletResponse response)
+    throws ServletException, IOException 
+  {
+
+    try {
+
+      // Declaring a report.
+      MasterReport report = new MasterReport();
+
+      // Defining the connection provider.
+      Connection connection = DriverManager.getConnection("jdbc:hsqldb:./resources/sampledata/sampledata", "pentaho_user", "password");
+      StaticConnectionProvider provider = new StaticConnectionProvider(connection);
+
+      // Defining the query.
+      SQLReportDataFactory dataFactory = new SQLReportDataFactory(provider);
+      String sqlQuery = "SELECT PRODUCTNAME, QUANTITYINSTOCK FROM PRODUCTS";
+      dataFactory.setQuery("default", sqlQuery);
+      report.setDataFactory(dataFactory);
+
+      // Getting the item band to host the elements.
+      ItemBand itemBand = report.getItemBand();
+
+      // Adding a text field for the product name, added to the item band.
+      TextFieldElementFactory textFactory = new TextFieldElementFactory(); 
+      textFactory.setFieldname("PRODUCTNAME");
+      textFactory.setX(1f);
+      textFactory.setY(1f); 
+      textFactory.setMinimumWidth(200f);
+      textFactory.setMinimumHeight(20f);
+      Element nameField = textFactory.createElement(); 
+      itemBand.addElement(nameField);
+
+      // Adding a number filed with the total cost of the products.
+      NumberFieldElementFactory numberFactory = new NumberFieldElementFactory();
+      numberFactory.setFieldname("QUANTITYINSTOCK");
+      numberFactory.setX(201f);
+      numberFactory.setY(1f);
+      numberFactory.setMinimumWidth(100f);
+      numberFactory.setMinimumHeight(20f);
+      Element totalCost = numberFactory.createElement();
+      itemBand.addElement(totalCost);
+
+      // Conversion to PDF and rendering.
+      response.setContentType("application/pdf");
+      PdfReportUtil.createPDF(report, response.getOutputStream());
+
+    }
+    catch (Exception e) 
+    {
+        e.printStackTrace();
+    }
+  }
+}
+
