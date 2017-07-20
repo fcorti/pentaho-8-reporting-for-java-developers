59382cca21a98f8a19a0d91dc72fefc428624a69
Francesco Corti <fcorti@gmail.com>
2017-07-20 23:55:18 +0200

    Added interactive-report.

---
+++ Chapter 14 - Building interactive reports/interactive-report/src/main/java/com/example/PentahoServlet2.java
@@ -0,0 +1,74 @@
+package com.example;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
+import org.pentaho.reporting.engine.classic.core.MasterReport;
+import org.pentaho.reporting.engine.classic.core.modules.output.table.html.HtmlReportUtil;
+import org.pentaho.reporting.libraries.resourceloader.Resource;
+import org.pentaho.reporting.libraries.resourceloader.ResourceManager;
+
+
+public class PentahoServlet2 extends HttpServlet 
+{
+
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public void init(
+    ServletConfig config) 
+    throws ServletException 
+  {
+
+    super.init(config);
+    ClassicEngineBoot.getInstance().start();
+
+  }
+
+  @Override
+  public void doGet(
+    HttpServletRequest request, 
+    HttpServletResponse response)
+    throws ServletException, IOException 
+  {
+    doPost(request, response);
+  }
+ 
+  @Override
+  public void doPost(
+    HttpServletRequest request, 
+    HttpServletResponse response)
+    throws ServletException, IOException 
+  {
+
+    try {
+
+      // Getting the report.
+      ResourceManager manager = new ResourceManager();
+      manager.registerDefaults();
+      Resource res = manager.createDirectly(
+        new URL("file:resources/interactive_report_2.prpt"),
+        MasterReport.class);
+      MasterReport report = (MasterReport) res.getResource();
+
+      // Mandatory parameter.
+      report.getParameterValues().put("LINE", request.getParameter("line"));
+
+      // Conversion to PDF and rendering.
+      response.setContentType("text/html");
+      HtmlReportUtil.createStreamHTML(report, response.getOutputStream());
+
+    }
+    catch (Exception e) 
+    {
+        e.printStackTrace();
+    }
+  }
+}
